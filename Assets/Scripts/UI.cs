4ff9233b89069cf1447b331eade2b67b6c57903f
Brenden Romanowski <brenden.romanowski@gmail.com>
2022-08-17 22:56:49 -0500

    Camera zooming UI

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
 using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
     private readonly float PAN_SPEED = 1f;
+    private readonly int ZOOM_SPEED = 2;
     [SerializeField] private Camera _camera;
+    [SerializeField] private PixelPerfectCamera _ppCamera;
     [SerializeField] private Button btnLeft;
     [SerializeField] private Button btnRight;
     [SerializeField] private Button btnUp;
     [SerializeField] private Button btnDown;
+    [SerializeField] private Button btnZoomOut, btnZoomIn;
     void Start()
     {
         btnLeft.onClick.AddListener(PanLeft);
         btnRight.onClick.AddListener(PanRight);
         btnUp.onClick.AddListener(PanUp);
         btnDown.onClick.AddListener(PanDown);
+        btnZoomOut.onClick.AddListener(ZoomOut);
+        btnZoomIn.onClick.AddListener(ZoomIn);
     }
 
     void PanLeft()
@@ -41,5 +47,17 @@ public class UI : MonoBehaviour
     {
         Vector3 pos = _camera.transform.position;
         _camera.transform.position = new Vector3(pos.x, pos.y - PAN_SPEED, pos.z);
+    }
+
+    void ZoomOut()
+    {
+        _ppCamera.refResolutionX *= ZOOM_SPEED;
+        _ppCamera.refResolutionY *= ZOOM_SPEED;
+    }
+
+    void ZoomIn()
+    {
+        _ppCamera.refResolutionX /= ZOOM_SPEED;
+        _ppCamera.refResolutionY /= ZOOM_SPEED;
     }
 }
