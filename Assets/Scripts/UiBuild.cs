a6e3ccee3396612e27e832510b77d795e1909320
Brenden Romanowski <brenden.romanowski@gmail.com>
2022-09-04 19:17:41 -0500

    Resolution

--- Assets/Scripts/UiBuild.cs
+++ Assets/Scripts/UiBuild.cs
@@ -5,30 +5,44 @@ using UnityEngine.UI;
 public class UIBuild : MonoBehaviour
 {
     [SerializeField] private List<Button> buildButtons;
-
+    [SerializeField] private List<GameTile> tiles;
+    [SerializeField] private Image selectedTileType;
+    private Dictionary<GameTile.TileTypes, GameTile> tileTypes;
     private void Start()
     {
+        tileTypes = new Dictionary<GameTile.TileTypes, GameTile>();
+        foreach (var tile in tiles)
+        {
+           tileTypes.Add(tile.type, tile); 
+        }
+        
         foreach (var btn in buildButtons)
         {
-            btn.onClick.AddListener(() => BuildSettings.selectedPiece = BtnToPiece(btn));
+            btn.onClick.AddListener(() => BtnOnClick(btn));
         }
     }
 
-    private BuildPiece BtnToPiece(Button btn)
+    private void BtnOnClick(Button btn)
+    {
+        BuildSettings.selectedPiece = tileTypes[BtnToPiece(btn)];
+        selectedTileType.sprite = BuildSettings.selectedPiece.sprite;
+    }
+
+    private GameTile.TileTypes BtnToPiece(Button btn)
     {
         switch (btn.name)
         {
             case "BtnBuildBlock":
-                return BuildPiece.BLOCK;
+                return GameTile.TileTypes.BLOCK;
             case "BtnBuildBlockHalf":
-                return BuildPiece.BLOCK_HALF;
+                return GameTile.TileTypes.BLOCK_HALF;
             case "BtnBuildBlockCorner":
-                return BuildPiece.BLOCK_CORNER;
+                return GameTile.TileTypes.BLOCK_CORNER;
             case "BtnBuildBlockRamp":
-                return BuildPiece.RAMP;
+                return GameTile.TileTypes.RAMP;
             default:
                 Debug.LogError("Invalid build button name");
-                return BuildPiece.BLOCK;
+                return GameTile.TileTypes.BLOCK;
         }
     }
 }
