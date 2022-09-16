ec0eb9ce066cf4f0e5e536c52b07cad9e5ffb8ea
Brenden Romanowski <brenden.romanowski@gmail.com>
2022-09-15 21:09:11 -0500

    Start menu

---
+++ Assets/Scripts/DataPark.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DataPark : ScriptableObject
+{
+    private string title;
+    private string author;
+    private string data;
+    
+    /*
+     * {
+     *     "tiles": [
+     *          "tileId": BLOCK,
+     *          "x": 0,
+     *          "y": 0,
+     *          "elevation": 0
+     *     ]
+     * }
+     */
+
+    public string TilesToJson(List<Tilemap> tilemaps)
+    {
+        return null;
+    }
+
+    public List<Tilemap> JsonToTilemaps(string json)
+    {
+        return null;
+    }
+}
