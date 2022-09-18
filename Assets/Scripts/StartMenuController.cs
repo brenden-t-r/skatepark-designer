a756b5c55b03a20c55b653b7f9211e1e1bf5d060
Brenden Romanowski <brenden.romanowski@gmail.com>
2022-09-18 14:27:54 -0500

    Park Menu Option Prefab

--- Assets/Scripts/StartMenuController.cs
+++ Assets/Scripts/StartMenuController.cs
@@ -1,26 +1,54 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StartMenuController : MonoBehaviour
 {
-    [SerializeField] private List<ParkMenuOption> parkMenuOptions;
     [SerializeField] private Button btnNew;
+    [SerializeField] private Transform viewportContent;
+    [SerializeField] private GameObject parkMenuOptionPrefab;
     
     void Start()
     {
-        foreach (var parkMenuOption in parkMenuOptions)
-        {
-            Button btnLoad = parkMenuOption.btnLoad;
-            Text parkName = parkMenuOption.textName;
-            btnLoad.onClick.AddListener(() => LoadPark(parkName.text));
-        }
-        
+        LoadSavedParks();
         btnNew.onClick.AddListener(NewPark);
+    }
+
+    void LoadSavedParks()
+    {
+        string parksJson = PlayerPrefs.GetString("Parks");
+        ParkDataSaves.ListWrapper<String> parks = parksJson != "" 
+            ? JsonUtility.FromJson<ParkDataSaves.ListWrapper<String>>(parksJson) 
+            : new ParkDataSaves.ListWrapper<string>();
+        Debug.Log(parks.list.Count);
+        for (int i = 0; i < parks.list.Count; i++)
+        {
+            String parkDataJson = PlayerPrefs.GetString(parks.list[i]);
+            Debug.Log(parkDataJson);
+            if (parkDataJson.Equals(""))
+            {
+                Debug.Log(parks.list[i] + " is empty.");
+                continue;
+            }
+            ParkData parkData = ScriptableObject.CreateInstance<ParkData>();
+            JsonUtility.FromJsonOverwrite(parkDataJson, parkData);
+            addParkMenuOption(i, parkData);
+        }
+    }
+
+    void addParkMenuOption(int index, ParkData parkData)
+    {
+        GameObject prefab = Instantiate(parkMenuOptionPrefab, viewportContent);
+        RectTransform rectTransform = prefab.GetComponent<RectTransform>();
+        rectTransform.anchorMin = new Vector2(0, 1);
+        rectTransform.anchorMax = new Vector2(1, 1);
+        rectTransform.sizeDelta = new Vector2(0, 0);
+        rectTransform.anchoredPosition = new Vector2(0, index * -120f);
+        ParkMenuOption option = prefab.GetComponentInChildren<ParkMenuOption>();
+        option.textName.text = parkData.title;
+        Button btnLoad = option.btnLoad;
+        btnLoad.onClick.AddListener(() => LoadPark(parkData));
     }
 
     public void NewPark()
@@ -38,6 +66,12 @@ public class StartMenuController : MonoBehaviour
         Debug.Log(json);
         ParkData parkData = ScriptableObject.CreateInstance<ParkData>();
         JsonUtility.FromJsonOverwrite(json, parkData);
+        ParkDataSaves.parkData = parkData;
+        SceneManager.LoadScene("EditorScene");
+    }
+
+    public void LoadPark(ParkData parkData)
+    {
         ParkDataSaves.parkData = parkData;
         SceneManager.LoadScene("EditorScene");
     }
