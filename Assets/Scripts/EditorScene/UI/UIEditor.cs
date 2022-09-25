cddc2dabd1dbec77bfd003c8adaf6b44c2322f8d
Brenden Romanowski <brenden.romanowski@gmail.com>
2022-09-25 13:34:16 -0500

    Extract class, UINavigation

--- Assets/Scripts/EditorScene/UI/UIEditor.cs
+++ Assets/Scripts/EditorScene/UI/UIEditor.cs
@@ -8,18 +8,9 @@ namespace EditorScene.UI
 {
     public class UIEditor : MonoBehaviour
     {
-        private readonly float PAN_SPEED = 1f;
-        private readonly int ZOOM_SPEED = 2;
-        [SerializeField] private Transform _camera;
-        [SerializeField] private PixelPerfectCamera _ppCamera;
-    
         // Park menu
         [SerializeField] private Text textParkName;
         [SerializeField] private Button btnSave, btnClear, btnExit;
-
-        // Navigation UI
-        [SerializeField] private Button btnLeft, btnRight, btnUp, btnDown;
-        [SerializeField] private Button btnZoomOut, btnZoomIn;
 
         // Save Menu
         [SerializeField] private GameObject saveMenuPanel;
@@ -28,86 +19,44 @@ namespace EditorScene.UI
         [SerializeField] private Button saveMenuBtnSave;
         [SerializeField] private Button saveMenuBtnCancel;
     
-        [SerializeField] private BuildController buildController;
-    
-        void Start()
+        //[SerializeField] private BuildController buildController;
+
+        private void Start()
         {
             btnSave.onClick.AddListener(SaveMenuOpen);
             btnClear.onClick.AddListener(ClearPark);
             btnExit.onClick.AddListener(ExitToMainMenu);
-            btnLeft.onClick.AddListener(PanLeft);
-            btnRight.onClick.AddListener(PanRight);
-            btnUp.onClick.AddListener(PanUp);
-            btnDown.onClick.AddListener(PanDown);
-            btnZoomOut.onClick.AddListener(ZoomOut);
-            btnZoomIn.onClick.AddListener(ZoomIn);
             saveMenuBtnSave.onClick.AddListener(SaveMenuSave);
             saveMenuBtnCancel.onClick.AddListener(SaveMenuCancel);
             textParkName.text = ParkDataSaves.parkData.title;
             saveMenuPanel.SetActive(false);
         }
 
-        void PanLeft()
+        private void ClearPark()
         {
-            Vector3 pos = _camera.transform.position;
-            _camera.position = new Vector3(pos.x - PAN_SPEED, pos.y, pos.z);
+            TilemapManager._instance.ClearMap();
         }
 
-        void PanRight()
-        {
-            Vector3 pos = _camera.transform.position;
-            _camera.position = new Vector3(pos.x + PAN_SPEED, pos.y, pos.z);
-        }
-    
-        void PanUp()
-        {
-            Vector3 pos = _camera.transform.position;
-            _camera.position = new Vector3(pos.x, pos.y + PAN_SPEED, pos.z);
-        }
-    
-        void PanDown()
-        {
-            Vector3 pos = _camera.transform.position;
-            _camera.position = new Vector3(pos.x, pos.y - PAN_SPEED, pos.z);
-        }
-
-        void ZoomOut()
-        {
-            _ppCamera.refResolutionX *= ZOOM_SPEED;
-            _ppCamera.refResolutionY *= ZOOM_SPEED;
-        }
-
-        void ZoomIn()
-        {
-            _ppCamera.refResolutionX /= ZOOM_SPEED;
-            _ppCamera.refResolutionY /= ZOOM_SPEED;
-        }
-
-        void ClearPark()
-        {
-            buildController.ClearMap();
-        }
-
-        void SaveMenuOpen()
+        private void SaveMenuOpen()
         {
             saveMenuPanel.SetActive(true);
         }
 
-        void SaveMenuCancel()
+        private void SaveMenuCancel()
         {
             saveMenuPanel.SetActive(false);
         }
 
-        void SaveMenuSave()
+        private void SaveMenuSave()
         {
             ParkDataSaves.parkData.title = saveMenuInputName.text;
             ParkDataSaves.parkData.author = saveMenuInputAuthor.text;
             textParkName.text = saveMenuInputName.text;
-            buildController.SaveMap();
+            TilemapManager._instance.SaveMap();
             saveMenuPanel.SetActive(false);
         }
 
-        void ExitToMainMenu()
+        private void ExitToMainMenu()
         {
             SceneManager.LoadScene("StartMenuScene");
         }
---
+++ Assets/Scripts/EditorScene/UI/UINavigation.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
+
+namespace EditorScene.UI
+{
+    
+    public class UINavigation : MonoBehaviour
+    {
+        private readonly float PAN_SPEED = 1f;
+        private readonly int ZOOM_SPEED = 2;
+        
+        private Transform cameraTransform;
+        private PixelPerfectCamera pixelPerfectCamera;
+        [SerializeField] private Button btnLeft, btnRight, btnUp, btnDown;
+        [SerializeField] private Button btnZoomOut, btnZoomIn;
+
+        private void Start()
+        {
+            Camera cam = Camera.main;
+            cameraTransform = cam.GetComponent<Transform>();
+            pixelPerfectCamera = cam.GetComponent<PixelPerfectCamera>();
+            btnLeft.onClick.AddListener(PanLeft);
+            btnRight.onClick.AddListener(PanRight);
+            btnUp.onClick.AddListener(PanUp);
+            btnDown.onClick.AddListener(PanDown);
+            btnZoomOut.onClick.AddListener(ZoomOut);
+            btnZoomIn.onClick.AddListener(ZoomIn);
+        }
+        
+        private void PanLeft()
+        {
+            Vector3 pos = cameraTransform.transform.position;
+            cameraTransform.position = new Vector3(pos.x - PAN_SPEED, pos.y, pos.z);
+        }
+
+        private void PanRight()
+        {
+            Vector3 pos = cameraTransform.transform.position;
+            cameraTransform.position = new Vector3(pos.x + PAN_SPEED, pos.y, pos.z);
+        }
+    
+        private void PanUp()
+        {
+            Vector3 pos = cameraTransform.transform.position;
+            cameraTransform.position = new Vector3(pos.x, pos.y + PAN_SPEED, pos.z);
+        }
+    
+        private void PanDown()
+        {
+            Vector3 pos = cameraTransform.transform.position;
+            cameraTransform.position = new Vector3(pos.x, pos.y - PAN_SPEED, pos.z);
+        }
+
+        private void ZoomOut()
+        {
+            pixelPerfectCamera.refResolutionX *= ZOOM_SPEED;
+            pixelPerfectCamera.refResolutionY *= ZOOM_SPEED;
+        }
+
+        private void ZoomIn()
+        {
+            pixelPerfectCamera.refResolutionX /= ZOOM_SPEED;
+            pixelPerfectCamera.refResolutionY /= ZOOM_SPEED;
+        }
+    }
+}
